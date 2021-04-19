1ce1129efe81c464004fa05aa2d6a7fd22d00ad0
Sourav Sahu <sahu.souravrb1999@gmail.com>
2021-04-19 20:54:21 +0530

    DQ7.java updated
    Properly indented the program.
    This increases the readability of program and added some useful comment lines.

--- DQ7.java
+++ DQ7.java
@@ -1,12 +1,27 @@
-import java.util.Scanner; public class DQ7 
-{ static int fact(int n) 
-{ if(n==0) 
-	return 1; 
-else return n*fact(n-1); }
-public static void main(String args[]) 
-{ Scanner in =new Scanner(System.in); 
-System.out.println("enter n value"); 
-int a =in.nextInt(); 
-int k =fact(a); 
-System.out.println(k); }
+// Program to find factorial of a number
+// Concept used:- Recursion
+
+import java.util.Scanner;
+public class DQ7 
+{ 
+	static int fact(int n) 
+	{ 
+		if(n==0) 
+			return 1; 
+		else 
+			return n*fact(n-1); 
+	}
+
+	public static void main(String args[]) 
+	{ 
+		Scanner in =new Scanner(System.in); 
+		System.out.println("Enter value of n>>"); 
+		int a = in.nextInt(); 
+		int k = fact(a); 
+		System.out.println(k); 
+		in.close();
+	}
 }
+
+// Time complexity = O(N)
+// Space complexity = O(N)
